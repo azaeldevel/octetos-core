75c5d3a1c57947a3ff6be8fec19b178a74a1d959
Azael Reyes <azael.devel@gmail.com>
2020-03-26 03:44:09 -0700

    Pricipalmente se agrego la licencia en los archivos.

--- src/semver-parser.h
+++ src/semver-parser.h
@@ -1,5 +1,26 @@
 #ifndef OCTETOS_CORE_SEMVER_PARSER_H
 #define OCTETOS_CORE_SEMVER_PARSER_H
+/**
+ * 
+ *  This file is part of octetos-core.
+ *  octetos-core is a C/C++ library to mainly provider implmetation Semver.
+ *  Copyright (C) 2018  Azael Reyes
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * */
+
 
 #include "common.h"
 
