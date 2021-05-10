4a5327d00228c3a482d1df3793d35221ffdc6a36
Azael Reyes <azael.devel@gmail.com>
2021-05-09 20:16:35 -0700

    minigc

--- src/develop.cc
+++ src/develop.cc
@@ -5,6 +5,8 @@
 
 #include "Mail.hh"
 #include "data.hh"
+#include "memory.hh"
+
 #define LENGTH 1000
 
 
@@ -31,5 +33,7 @@ int main()
 	auto stop2 = std::chrono::high_resolution_clock::now();
 	auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop2 - start2);
 	std::cout << duration1.count() << "/" << duration2.count() << "=" << duration1.count()/duration2.count() << "\n";
+	
+	octetos::core::DoubleBlock<int> minigc(1000);
 	
 }
