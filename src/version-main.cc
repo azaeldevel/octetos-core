fba70c643a99bb210a9dc36f090a5bf820f955b9
Azael <azael.devel@gmail.com>
2023-02-08 16:14:09 -0800

    save

--- src/version-main.cc
+++ src/version-main.cc
@@ -2,9 +2,14 @@
 #include<string.h>
 #include <string>
 #include <iostream>
-#include "config.h"
-//#include <glibmm/i18n.h>
 
+#ifdef __linux__
+    #include "config.h"
+#elif defined(_WIN32) || defined(_WIN64)
+
+#else
+    #error "Plataforma desconcida."
+#endif
 
 #include "Version.hh"
 
@@ -13,11 +18,11 @@ int main(int argc, char* argv[])
 	//bindtextdomain(GETTEXT_PACKAGE, OCTETOS_CORE_LOCALEDIR);
 	//bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
 	//textdomain(GETTEXT_PACKAGE);
-	
+
 	const char* verstr;
 	oct::core::v2::Semver ver;
 	bool ver_detected = false;
-	
+
 	if(argc >= 1)
 	{
 		verstr = (const char*)argv[1];
@@ -30,7 +35,7 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	
+
 	if(ver_detected)
 	{
 		for(int i = 1; i < argc; i++)
@@ -41,19 +46,19 @@ int main(int argc, char* argv[])
 			}
 			else if(strcmp("-m", argv[i]) == 0 or strcmp("--minor", argv[i]) == 0)
 			{
-				std::cout << ver.getMinor();				
+				std::cout << ver.getMinor();
 			}
 			else if(strcmp("-p", argv[i]) == 0 or strcmp("--patch", argv[i]) == 0)
 			{
-				std::cout << ver.getPatch();				
+				std::cout << ver.getPatch();
 			}
 			else if(strcmp("-s", argv[i]) == 0 or strcmp("--stage", argv[i]) == 0 or strcmp("--pre-release", argv[i]) == 0)
 			{
-				std::cout << ver.getPrerelease ();				
+				std::cout << ver.getPrerelease ();
 			}
 			else if(strcmp("-b", argv[i]) == 0 or strcmp("--build", argv[i]) == 0)
 			{
-				std::cout << ver.getBuild();				
+				std::cout << ver.getBuild();
 			}
 		}
 	}
