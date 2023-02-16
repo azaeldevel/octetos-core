6df03440f94771e23f1c0a8c84492fbbc9935693
Azael <azael.devel@gmail.com>
2023-02-16 14:54:08 -0800

    save

--- src/arrays.hh
+++ src/arrays.hh
@@ -4,182 +4,30 @@
 
 #include <vector>
 #include <iostream>
+#include <initializer_list>
 
 
-namespace oct::core::v3::arrays
+namespace oct::core::v3
 {
-
 
 template<typename T>
-concept SortableData = requires (T t1,T t2)
-{
-	t1 < t2;
-	t1 > t2;
-	//t1 == t2;
-	t1.print(std::cout);
-};
-template<SortableData D,typename I> void copy(I begin, I end,D* in,D* out)
-{
-	for(I i = begin; i < end; i++)
-	{
-		out[i] = in[i];
-	}
-}
-template<SortableData D,typename I> void merge(D* in,I begin, I middle, I end,D* out)
-{
-			I i = begin;
-			I j = middle;
-			I k = begin;
-
-			while(i < middle && j <= end)
-			{
-				if (in[i] < in[j])
-				{
-						//std::cout << " : Cierto\n";
-						out[k] = in[i];
-						i++;
-				}
-				else
-				{
-						//std::cout << " : Falso\n";
-						out[k] = in[j];
-						j++;
-				}
-				k++;
-			}
-			while(i < middle )
-			{
-					//std::cout << "\tvaciando " << in[i]->index << "\n";
-					out[k] = in[i];
-					i++;
-					k++;
-			}
-			while(j <= end )
-			{
-					//std::cout << "\tvaciando " << in[j]->index << "\n";
-					out[k] = in[j];
-					j++;
-					k++;
-			}
-}
-template<SortableData D,typename I> void split(D* out,I begin, I end,D* in)
-{
-	if (end - begin < 1) return;
-				
-	I middle = begin + (end - begin) / 2;
-
-	split(in,begin,middle,out);
-	split(in,middle + 1,end,out);
-
-	merge(out,begin,middle+1,end,in);
-}
-template<SortableData D,typename I> void sort(D* data, I length)
-{
-	D work[length];	
-
-	copy(I(0),length,data,work);
-	split(work,I(0),length - 1,data);
-}
-
-template<typename D>
-class array 
+class array
 {
 public:
-	
-protected:
-	
-};
+    constexpr array() = delete;
+    constexpr array(std::initializer_list<T> list) : _size(list.size())
+    {
 
-template<typename D>
-class Wrapper : public array<D>
-{
+    }
+    constexpr array(size_t reserved) : _size(reserved)
+    {
+
+    }
+
 public:
-	Wrapper(D* d,size_t s) : head(d),size(s)
-	{
-		std::cout << "Wrapper(D* d,size_t s)\n";
-	}
-	
-
-
-	D& operator [](size_t index)
-	{
-		if(index < size) return &head[index];
-
-		throw std::out_of_range("El indice esta fuera de rango");
-	}
-	const D* operator [](size_t index) const
-	{
-		if(index < size) return &head[index];
-
-		throw std::out_of_range("El indice esta fuera de rango");
-	}
-	size_t get_size()const
-	{
-		return size;
-	}
-
-	void print(std::ostream& out)const
-	{
-		for(size_t i = 0; i < size; i++)
-		{
-			head[i].print(out);
-		}
-	}
-
-private:
-	D* head;
-	size_t size;
+    const size_t _size;
+    //T _data[_size];
 };
-
-
-
-template<SortableData D,typename... K>
-class Table : public Wrapper<D>
-{
-public:
-	Table(D* d,size_t s) : head(d),size(s)
-	{
-		std::cout << "Wrapper(D* d,size_t s)\n";
-	}
-	
-
-	virtual D* find(K... k)
-	{
-		return &head[0];
-	}
-	virtual const D* find(K... k) const
-	{
-		if(sizeof...(K) == 0) return NULL;
-		
-		
-		
-		return &head[0];
-	}
-
-	D& operator [](size_t index)
-	{
-		if(index < size) return &head[index];
-
-		throw std::out_of_range("El indice esta fuera de rango");
-	}
-	const D* operator [](size_t index) const
-	{
-		if(index < size) return &head[index];
-
-		throw std::out_of_range("El indice esta fuera de rango");
-	}
-
-	void sort()
-	{
-		oct::core::v3::arrays::sort(head,size);
-	}
-private:
-	D* head;
-	size_t size;
-};
-
-
-
 
 }
 
---
+++ src/develop-arrays.cc
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+#include "arrays.hh"
+
+namespace core_here = oct::core::v3;
+
+
+
+int main(int argc, char* argv[])
+{
+    constexpr core_here::array numbers = {1,2,3,4,5,6,7,8,9};
+
+	return EXIT_SUCCESS;
+}
