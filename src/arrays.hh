59f10980826073c0809c6d2c2a32a6cdd1d6a7c3
Azael <azael.devel@gmail.com>
2023-02-16 16:30:51 -0800

    save

--- src/arrays.hh
+++ src/arrays.hh
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <initializer_list>
 
+#include "Exception.hh"
 
 namespace oct::core::v3
 {
@@ -15,18 +16,49 @@ class array
 {
 public:
     constexpr array() = delete;
-    constexpr array(std::initializer_list<T> list) : _size(list.size())
+    constexpr array(size_t reserved) : _size(reserved),_begin(new T)
+    {
+    }
+    constexpr array(std::initializer_list<T> list) : _size(list.size()),_begin(new T)
+    {
+        for(size_t i = 0; i < _size; i++)
+        {
+            _begin[i] = list.begin()[i];
+        }
+    }
+    constexpr array(const array& a) : _size(a._size),_begin(new T)
     {
 
     }
-    constexpr array(size_t reserved) : _size(reserved)
+    const T* begin() const
     {
-
+        return _begin;
     }
+    const T* end() const
+    {
+        return _begin + (_size - 1);
+    }
+    T& operator [](size_t i)
+    {
+        if(i < _size) return _begin[i];
 
-public:
-    const size_t _size;
-    //T _data[_size];
+        throw exception("Indice fuera de rango");
+    }
+/*#if defined(__GNUC__)
+
+#elif defined(__clang__)
+    #error "Sin emplementar"
+#elif defined(_MSC_VER)
+    #error "Sin emplementar"
+#else
+    #error "Compilador desconocido"
+#endif
+*/
+private:
+
+private:
+    size_t _size;
+    T* _begin;
 };
 
 }
--- src/develop-arrays.cc
+++ src/develop-arrays.cc
@@ -9,7 +9,11 @@ namespace core_here = oct::core::v3;
 
 int main(int argc, char* argv[])
 {
-    constexpr core_here::array numbers = {1,2,3,4,5,6,7,8,9};
+    core_here::array<int> numbers = {-1,0,1,2,3,4,5,6,7};
+    for(auto e : numbers)
+    {
+        std::cout << "Number : " << e << "\n";
+    }
 
 	return EXIT_SUCCESS;
 }
