d4db0c1dfeed7bb31cd686b560ae9036e6adc38f
Azael R <azael.devel@gmail.com>
2023-06-17 21:17:40 -0700

    fixed : fail on cimpilation time

--- src/Exception-v3.hh
+++ src/Exception-v3.hh
@@ -75,62 +75,6 @@ private:
 	void copy(const char*);
 };
 
-class exception
-{
-public:
-	typedef int errono;
-	static const errono err_none = 0;
-
-public:
-	exception(const exception&)noexcept = default;
-	exception(exception&&) noexcept = default;
-
-
-	exception(const std::source_location& location = std::source_location::current())noexcept : _code(err_none), _code_flag(false), _location(location)
-	{
-	}
-	exception(errono code, const std::source_location& location = std::source_location::current())noexcept : _code(code), _code_flag(true), _location(location)
-	{
-	}
-	exception(errono code, const std::string& message, const std::source_location& location = std::source_location::current())noexcept : _code(code), _code_flag(true), _message(message), _location(location)
-	{
-	}
-	exception(const std::string& message, const std::source_location& location = std::source_location::current())noexcept : _code(err_none), _code_flag(false), _message(message), _location(location)
-	{
-	}
-	virtual ~exception()noexcept
-	{
-	}
-
-	inline errono code()const
-	{
-		return _code;
-	}
-
-	virtual const char* what() const noexcept
-	{
-		if (description.empty())
-		{
-			description = _location.file_name();
-			description += ":";
-			description += std::to_string(_location.line()) + " ";
-			if (_code_flag) description += "- " + std::to_string(_code);
-			if (not _message.empty()) description += "\n\t" + _message;
-		}
-
-		return description.c_str();
-	}
-
-protected:
-	errono _code;
-	bool _code_flag;
-	std::string _message;
-	std::source_location _location;
-
-private:
-	mutable std::string description;
-};
-
 
 }
 
