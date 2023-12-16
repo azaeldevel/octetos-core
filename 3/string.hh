94e82f2a9d1778bba177742fc621fc352d7aa237
Azael <azael.devel@gmail.com>
2023-12-15 18:29:14 -0800

    save

---
+++ 3/string.hh
@@ -0,0 +1,17 @@
+
+#ifndef OCTETOS_CORE_V3_STRING
+#define OCTETOS_CORE_V3_STRING
+
+#include <string>
+#include <vector>
+
+namespace oct::core::v3
+{
+
+    void copy(const std::string& from,const std::string& to,bool include,std::vector<char>);
+
+}
+
+
+#endif
+
