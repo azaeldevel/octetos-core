b52d43ea1a97ef5c7433be95958b78b616432df0
Azael R <azael.devel@gmail.com>
2023-12-25 11:37:13 -0800

    save

--- 3/string.cc
+++ 3/string.cc
@@ -22,15 +22,6 @@
 namespace oct::core::v3
 {
 
-    std::string toupper(const std::string& from)
-    {
-        std::string to = from;
-        for(size_t i = 0; i < to.size(); i++)
-        {
-            to[i] = std::toupper(to[i]);
-        }
 
-        return to;
-    }
 
 }
