8d5ed4e77677e363933cf871cef5f590900b1a51
Azael R <azael.devel@gmail.com>
2023-12-29 00:54:34 -0800

    save

--- 3/string.hh
+++ 3/string.hh
@@ -8,14 +8,13 @@
 namespace oct::core::v3
 {
 
-    std::string toupper(const std::string& from)
+    template<class T> std::string toupper(const T& from)
     {
-        std::string to = from;
+        T to = from;
         for(size_t i = 0; i < to.size(); i++)
         {
             to[i] = std::toupper(to[i]);
         }
-
         return to;
     }
 
