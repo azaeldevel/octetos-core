e87ae8df4802b7f78f0e62532c05509aa77d1814
Azael <azael.devel@gmail.com>
2024-01-19 00:14:45 -0800

    save

--- tools/deps-dll.sh
+++ tools/deps-dll.sh
@@ -2,22 +2,30 @@
 
 if [ -z "$1" ]
 then
-	echo "deps-dll 'dll' 'dir to copy'"
+	echo "Usage:\tdeps-dll [library] [directory]"
 	exit
 fi
 if [ -z "$2" ]
 then
-	echo "deps-dll 'dll' 'dir to copy'"
+	echo "Usage:\tdeps-dll [library] [directory]"
 	exit
 fi
 
 list=`ntldd $1`
 readarray -t lines <<<"$list"
 #echo ${lines[0]}
+mkdir $2
 for line in "${lines[@]}"
 do
-  #echo "Dep : $line"
-  dll=`sed 's/[^"]*\(C:[^"]*.dll\) ([^"]*)/\1/' <<< "$line"`
-  mkdir $2
-  cp $dll $2
+	dll=`sed 's/[^"]*\(C:[^"]*.dll\) ([^"]*)/\1/' <<< "$line"`
+	case $dll in
+		"C:\Windows\SYSTEM32\ADVAPI32.dll")
+			continue
+		;;
+		"C:\Windows\SYSTEM32\KERNEL32.dll")
+			continue
+		;;
+	esac
+	echo $dll
+	cp $dll $2
 done
