6abb06bd93398b43d9ab58c35673cb82221e2c0d
Azael <azael.devel@gmail.com>
2024-01-18 23:59:30 -0800

    save

--- tools/deps-dll.sh
+++ tools/deps-dll.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+then
+	echo "deps-dll 'dll' 'dir to copy'"
+	exit
+fi
+if [ -z "$2" ]
+then
+	echo "deps-dll 'dll' 'dir to copy'"
+	exit
+fi
+
 list=`ntldd $1`
 readarray -t lines <<<"$list"
 #echo ${lines[0]}
@@ -7,6 +18,6 @@ for line in "${lines[@]}"
 do
   #echo "Dep : $line"
   dll=`sed 's/[^"]*\(C:[^"]*.dll\) ([^"]*)/\1/' <<< "$line"`
-  mkdir -p $2
+  mkdir $2
   cp $dll $2
 done
