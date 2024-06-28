3bc0462e78ec6c605e7f7d47af911a818fbbeb1e
Azael R. <azael.devel@gmail.com>
2024-06-27 21:32:28 -0700

    save

--- tests/nodes-develop.cc
+++ tests/nodes-develop.cc
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
 
     core::Number<float> num1(5.0);
-    core::Number<float> num2(1.3);
+    core::Number<float> num2(1.9);
     core::Addition<float> op1(num1,num2);
     std::cout <<"Developing...\n";
     float result = op1;
