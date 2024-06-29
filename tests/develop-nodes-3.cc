440f4683af195594d0dab277fbd23430e3b88d1c
Azael R. <azael.devel@gmail.com>
2024-06-29 00:37:18 -0700

    save

--- tests/develop-nodes-3.cc
+++ tests/develop-nodes-3.cc
@@ -1,9 +1,3 @@
-#include <cstdlib>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <random>
-#include <chrono>
 
 #include "nodes.hh"
 
@@ -11,12 +5,7 @@ namespace core = oct::core::v3;
 
 int main(int argc, char *argv[])
 {
-    core::nodes::Number num1(10);
-    core::nodes::Number num2(33);
-    core::nodes::Number num3(24.36);
-    core::nodes::Operation op1('+',num1,num2);
-    core::nodes::Operation op2('*',num3,op1);
-    op2.print(std::cout);
+
 
     return EXIT_SUCCESS;
 }
