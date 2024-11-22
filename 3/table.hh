1891a9b3f330b0d0e25437af8200ac0b18ea6a98
Azael R. <azael.devel@gmail.com>
2024-11-21 21:10:10 -0800

    save

--- 3/table.hh
+++ 3/table.hh
@@ -23,6 +23,50 @@ namespace oct::core::v3
 #pragma GCC diagnostic pop
 
 
+        constexpr T* operator [](I const& i)
+        {
+            return data[i];
+        }
+        constexpr const T* operator [](I const& i) const
+        {
+            return data[i];
+        }
+        constexpr T& at(I const& i,I const& j)
+        {
+            if(i < R) if(j < C) return data[i][j];
+
+            throw std::out_of_range("Indice fuera de rango");
+        }
+        constexpr T const& at(I const& i,I const& j) const
+        {
+            if(i < R) if(j < C) return data[i][j];
+
+            throw std::out_of_range("Indice fuera de rango");
+        }
+        constexpr operator T*()
+        {
+            return (T*)&data;
+        }
+        constexpr operator const T*() const
+        {
+            return (const T*)&data;
+        }
+#ifdef __cpp_multidimensional_subscript
+        constexpr T& operator [](I const& i,I const& j)
+        {
+            if(i < R) if(j < C) return data[i][j];
+
+            throw std::out_of_range("Indice fuera de rango");
+        }
+        constexpr T const& operator [](I const& i,I const& j)const
+        {
+            if(i < R) if(j < C) return data[i][j];
+
+            throw std::out_of_range("Indice fuera de rango");
+        }
+#endif
+
+
     };
 }
 
